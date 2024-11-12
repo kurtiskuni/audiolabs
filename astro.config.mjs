aee447d31cf42e71b2debcedf02881321fce66bb
Kurtis <kurtiskuni@gmail.com>
2024-11-12 11:18:16 +1100

    yes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
+import netlify from '@astrojs/netlify';
 
-import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output: 'hybrid',
+  output: 'hybrid', // or 'hybrid' if needed
+  adapter: netlify(),
 });
